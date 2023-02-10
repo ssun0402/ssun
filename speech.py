e76aebfdf1c976fbd4297bee194c0251d1094df2
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-02-10 17:56:58 +0900

    Update speech.py

--- speech.py
+++ speech.py
@@ -6,9 +6,11 @@ r = sr.Recognizer()
 with sr.Microphone() as source :
     
     # 마이크로부터 오디오 읽기
-    audio_data = r.record(source, duration = 5)
     print("인식중.......")
+    audio_data = r.record(source, duration = 10)
     
-    # 음성을 문자열로 전화
+    
+    # 음성을 문자열로 전환
     text = r.recognize_google(audio_data)
+    print(".....음성 데이터 -> 텍스트.....")
     print(text)
