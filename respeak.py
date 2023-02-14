12fbf31eb329c828c396da5f29525fd430e6d8b1
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-02-14 17:00:27 +0900

    Create respeak.py

---
+++ respeak.py
@@ -0,0 +1,36 @@
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
+
+# 음성인식 객체 생성
+r = sr.Recognizer()
+
+with sr.Microphone() as source :
+    print("음성을 말해주세요!")
+    
+    # 스피커로부터 텍스트 읽기
+    txt = "음성을 말해주세요!"
+    tts_kr = gTTS(txt, lang = 'ko', slow = False)
+    tts_kr.save("voice.wav")
+    playsound.playsound("voice.wav")
+    
+    # 마이크로부터 오디오 읽기
+    audio_data = r.record(source, duration = 5)
+    
+try:
+    # 음성을 문자열로 전환
+    # 구글 API로 인식 (하루에 50회 제한)
+    text = r.recognize_google(audio_data, language = 'ko')
+    print("<음성을 문자로 변환한 값을 아래에 표시했습니다.>")
+    print(text)
+    print(type(text))
+    
+    
+    
+# 음성 인식 실패한 경우
+except sr.UnknownValueError:
+    print("인식 실패")    
+    
+# Crtl + c 누르면 음성 인식 멈춤
+#except KeyboardInterrupt: 
+    #pass
