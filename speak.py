cd6c5e0695da81bb4813457751877bf6b8ae8de2
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-02-14 15:27:23 +0900

    Create speak.py

---
+++ speak.py
@@ -0,0 +1,18 @@
+from gtts import gTTS
+import playsound
+
+def speak_ko():
+    txt = "음성을 말해주세요!"
+    tts_kr = gTTS(text = txt, lang = 'ko', slow = False)
+    tts_kr.save("voice.wav")
+    playsound.playsound("voice.wav")
+    
+def speak_en():
+    txt = "hello! nice to meet you!"
+    tts_kr = gTTS(text = txt, lang = 'en', slow = False)
+    tts_kr.save("voice.wav")
+    playsound.playsound("voice.wav")
+    
+if __name__ == "__main__" :
+    speak_ko()
+    speak_en()
