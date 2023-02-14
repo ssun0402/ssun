161a8c27b259422cea438aff30576a41c0e16af5
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-02-14 17:52:28 +0900

    Update respeak1.py

--- respeak1.py
+++ respeak1.py
@@ -25,15 +25,22 @@ while True:
         # 음성을 문자열로 전환
         # 구글 API로 인식 (하루에 50회 제한)
         text = r.recognize_google(audio_data, language = 'ko')
-        print("<음성을 문자로 변환한 값을 아래에 표시했습니다.>")
-        print(text)
     
-        # 인식된 음성에 대한 대답
-        print(text + "라고 말했습니다.")
-        txt = text + "라고 말했습니다."
-        tts_kr = gTTS(txt, lang = 'ko', slow = False)
-        tts_kr.save("voice.wav")
-        playsound.playsound("voice.wav")
+        if (text == "마이클 젯슨") : 
+            # 스피커로부터 텍스트 읽기
+            print("네 음성을 말해주세요!")
+            txt = "네 음성을 말해주세요!"
+            tts_kr = gTTS(txt, lang = 'ko', slow = False)
+            tts_kr.save("voice.wav")
+            playsound.playsound("voice.wav")
+            
+        elif (text == "음성 인식 꺼줘") :
+            print(text)
+            pass
+        
+        else : 
+            print("<음성을 문자로 변환한 값을 아래에 표시했습니다.>")
+            print(text)
     
     # 음성 인식 실패한 경우
     except sr.UnknownValueError:
