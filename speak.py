c37a59f9ab78f836928758259e10a32ca8a26f74
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-02-14 16:44:18 +0900

    Update speak.py

--- speak.py
+++ speak.py
@@ -2,12 +2,14 @@ from gtts import gTTS
 import playsound
 
 def speak_ko():
+    print("음성을 말해주세요!")
     txt = "음성을 말해주세요!"
     tts_kr = gTTS(text = txt, lang = 'ko', slow = False)
     tts_kr.save("voice.wav")
     playsound.playsound("voice.wav")
     
 def speak_en():
+    print("hello! nice to meet you!")
     txt = "hello! nice to meet you!"
     tts_kr = gTTS(text = txt, lang = 'en', slow = False)
     tts_kr.save("voice.wav")
