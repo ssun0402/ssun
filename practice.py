3d42be8312f1bba959599c92c6c05978967af0dc
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-02-15 17:52:22 +0900

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,13 @@
+text = "613으로 가줘, 어디로 가줘"
+location = ['으로', '로', '에', '을', '를']
+
+
+for text in location:
+    
+
+print(location.split('으로'))
+
+location = location.split('으로')
+
+print(location)
+print(type(location[0]))
