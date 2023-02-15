cc4583f545ea7e2e4bff60e9aa493a70904717ac
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-02-15 15:12:37 +0900

    Update respeak1.py

--- respeak1.py
+++ respeak1.py
@@ -3,33 +3,33 @@
 import speech_recognition as sr
 from gtts import gTTS
 import playsound
-import time
 
-print("음성을 입력하세요!")
-
-while True:
+# 이름 인식 코드
+# 음성인식 끝내는 코드
+def speak_jetson():
+    
     # 음성인식 객체 생성
     r = sr.Recognizer()
-
+    
     with sr.Microphone() as source :
         
-        
-        
         # 마이크로부터 오디오 읽기
-        audio_data = r.record(source, duration = 5)
-    
-    try:
+        print('"젯슨"을 불러주세요!')
+        audio_data = r.record(source, duration = 2)
+        
         # 음성을 문자열로 전환
         # 구글 API로 인식 (하루에 50회 제한)
         text = r.recognize_google(audio_data, language = 'ko')
-    
-        if (text == "마이클 잭슨") : 
+        
+        if(text == "잭슨") :
+            
             # 스피커로부터 텍스트 읽기
-            print("네 음성을 말해주세요!")
-            txt = "네 음성을 말해주세요!"
+            print("네! 음성을 말해주세요!")
+            txt = "네! 음성을 말해주세요!"
             tts_kr = gTTS(txt, lang = 'ko', slow = False)
             tts_kr.save("voice.wav")
             playsound.playsound("voice.wav")
+            return respeak
             
         elif (text == "음성 인식 꺼줘") :
             print("음성인식을 끄겠습니다.")
@@ -39,9 +39,29 @@ while True:
             playsound.playsound("voice.wav")
             pass
         
-        else : 
-            print("<음성을 문자로 변환한 값을 아래에 표시했습니다.>")
-            print(text)
+def respeak():
+    # 음성인식 객체 생성
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source :
+    
+        # 마이크로부터 오디오 읽기
+        audio_data = r.record(source, duration = 5)
+        
+    try:
+        # 음성을 문자열로 전환
+        # 구글 API로 인식 (하루에 50회 제한)
+        text = r.recognize_google(audio_data, language = 'ko')
+        print("<음성을 문자로 변환한 값을 아래에 표시했습니다.>")
+        print(text)
+    
+        # 인식된 음성에 대한 대답
+        print(text + "라고 말했습니다.")
+        txt = text + "라고 말했습니다."
+        tts_kr = gTTS(txt, lang = 'ko', slow = False)
+        tts_kr.save("voice.wav")
+        playsound.playsound("voice.wav")
+        return speak_jetson
     
     # 음성 인식 실패한 경우
     except sr.UnknownValueError:
@@ -50,3 +70,4 @@ while True:
         tts_kr = gTTS(txt, lang = 'ko', slow = False)
         tts_kr.save("voice.wav")
         playsound.playsound("voice.wav")
+        return respeak
