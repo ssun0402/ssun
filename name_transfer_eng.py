f5017856e8167e99602aaeee2c1ee2aed8c72a66
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-03-07 19:31:56 +0900

    Update name_transfer_eng.py

--- name_transfer_eng.py
+++ name_transfer_eng.py
@@ -1,11 +1,11 @@
 # 분리된 텍스트 중 이름 부분을 영어로 변경
 
-text = ['물건', '명현', '혜선', '태언', '희웅', '613']
-name = ['명현', '태언', '혜선', '희웅']
+text = ['물건', '강명현', '명현', '혜선', '태언', '희웅', '613']
+name = ['강명현', '명현', '태언', '혜선', '희웅']
 
 for i, word in enumerate(text) :
     if word in name :
-        if word == '명현' :
+        if (word == '명현')  | (word == '강명현') :
             text[i] = 'myung hyun'
             
         elif word == '혜선' :
